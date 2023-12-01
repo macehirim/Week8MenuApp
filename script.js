dc6e9ab712ded1716c23bb42564a373b3f0cdb01
Marcus <marcus@Marcuss-MacBook-Air.local>
2023-11-30 21:11:04 -0700

    Updated Files

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 class Player {
-    constructor(firstName, lastName, position){
-        this.firstName = firstName;
-        this.lastName = lastName;
+    constructor(fullName, position){
+        this.fullName = fullName;
         this.position = position;
     }
 }
 
 class Coach extends Player {
-    constructor(firstName, lastName) {
-        super(firstName, lastName);
+    constructor(fullName) {
+        super(fullName);
     }
 }
 
 class Menu {
+    constructor (players) {
+        this.team = players;
+    }
 start() {
     let selection = this.mainMenu();
     while (selection !=0) {
         switch (selection) {
             case '1':
-            this.playerIntro(player1);
+            this.playerIntro(this.team[0]); // This passes the second element of the team array into the playerIntro method
             break;
             case '2':
             this.playerIntro(player2);
@@ -33,19 +35,27 @@ start() {
             this.playerIntro(player5);
             break;
             case '6':
-            console.log(this.coachIntro(coach));
+            this.coachIntro(coach);
+            break;
+            case '7':
+            this.replaceSelection();
             break;
             default:
             selection = '0';
-        } selection = this.mainMenu(); 
+        } selection = this.mainMenu();
     } alert('Goodbye!');
 }    
     
-    playerIntro () { 
-        return `Starting at the ${this.position} position, introducing ${this.firstName} ${this.lastName}!`;
+// playerIntro introduces the players that have been passed through the method via an alert
+    playerIntro (player) { 
+        // console.log(`Is this working`, player.fullName) - tested if selection was working
+        return alert (`Starting at the ${player.position} position, introducing ${player.fullName}!`);
     }
-    coachIntro () {
-        return `Your team is coached by, ${this.firstName} ${this.lastName}!`;
+
+// coachIntro introduces the coach that has been passed through the method via an alert
+    coachIntro (coach) {
+        // console.log(`Does this work too?`, coach.fullName, coac)
+        return alert(`Your team is coached by ${coach.fullName}!`);
     }                            
     mainMenu() {
         return prompt (
@@ -55,16 +65,85 @@ start() {
             3) Small Forward
             4) Power Forward
             5) Center
-            6) Coach`
+            6) Coach
+            7) Replace Player or Coach
+            0) Exit`
         );
     }
+    // replacePrompt () {
+    //     return prompt (`
+    //     1) Replace Point Guard 
+    //     2) Replace Shooting Guard
+    //     3) Replace Small Forward
+    //     4) Replace Power Forward
+    //     5) Replace Center
+    //     6) Replace Coach
+    //     0) Exit`
+    //     )
+    // }
+    replaceMenu () {
+        return prompt (`
+        1) Replace Point Guard 
+        2) Replace Shooting Guard
+        3) Replace Small Forward
+        4) Replace Power Forward
+        5) Replace Center
+        6) Replace Coach
+        7) Go Back`
+        ) 
+    }
+    replaceSelection () {
+    let selection = this.replaceMenu();
+    while (selection !=0) {
+        switch (selection) {
+            case '1':
+                let newName1 = this.replaceNamePrompt();
+                this.team[0].fullName = newName1;
+                selection = 0;
+                break;
+            case '2':
+                let newName2 = this.replaceNamePrompt();
+                this.team[1].fullName = newName2;
+                selection = 0;
+                break;
+            case '3':
+                let newName3 = this.replaceNamePrompt();
+                this.team[2].fullName = newName3;
+                selection = 0;
+                break;
+            case '4':
+                let newName4 = this.replaceNamePrompt();
+                this.team[3].fullName = newName4;
+                selection = 0;
+                break;
+            case '5':
+                let newName5 = this.replaceNamePrompt();
+                this.team[4].fullName = newName5;
+                selection = 0;
+                break;
+            case '6':
+                let newName6 = this.replaceNamePrompt();
+                this.team[5].fullName = newName6;
+                selection = 0;
+                break;
+            default:
+                selection = 0;
+        } alert ('Returning to Main Menu')
+    }
+    }
+    replaceNamePrompt () {
+       return prompt (`Enter Name`)
+    }
+
 }
 
-let player1 = new Player('Magic', 'Johnson');
-let player2 = new Player('Kobe', 'Bryant', 'Shooting Guard');
-let player3 = new Player('Lebron', 'James', 'Small Forward');
-let player4 = new Player('Pau', 'Gasol');
-let player5 = new Player('Shaquille', "O'neal")
-let coach = new Coach('Phil', 'Jackson');
-let menu = new Menu();
+
+let player1 = new Player('Magic Johnson', 'Point Guard');
+let player2 = new Player('Kobe Bryant', 'Shooting Guard');
+let player3 = new Player('Lebron James', 'Small Forward');
+let player4 = new Player('Pau Gasol', 'Power Forward');
+let player5 = new Player("Shaquille O'Neal", 'Center') // O'Neal in double quotes
+let coach = new Coach('Phil Jackson');
+let team = [player1, player2, player3, player4, player5, coach]
+let menu = new Menu(team);
 menu.start();
